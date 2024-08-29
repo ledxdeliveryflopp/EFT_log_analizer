08982773b0a6170bbebe076b00b3d383ef045a17
ledxdeliveryflopp <korstim18@gmail.com>
2024-08-29 19:12:43 +0700

    add check previous log

--- src/widgets/log_search_widget.py
+++ src/widgets/log_search_widget.py
@@ -5,25 +5,75 @@ from PySide6.QtWidgets import QFileDialog
 from loguru import logger
 
 
-class SearchLogWidget(QtWidgets.QTableWidget):
+class ServerAnalyzeWidget(QtWidgets.QWidget):
 
     def __init__(self):
         super().__init__()
         self.help_text_status = False
-        self.text = QtWidgets.QLabel(alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.button = QtWidgets.QPushButton("Найти log файл")
+        self.last_log = None
+
         self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.button, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+
+        self.result_text = QtWidgets.QLabel(alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
         self.help_text = QtWidgets.QLabel("название лога: network-connection",
                                           alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.layout.addWidget(self.help_text)
-        self.button.clicked.connect(self.get_server_info)
+
+        self.search_log_button = QtWidgets.QPushButton("Найти log файл")
+        self.last_log_button = QtWidgets.QPushButton("Получить информацию с последнего лога")
+
+        self.last_log_button.clicked.connect(self.get_last_log)
+        self.search_log_button.clicked.connect(self.get_server_info)
+
+        self.layout.addWidget(self.search_log_button, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.layout.addWidget(self.help_text, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.layout.addWidget(self.last_log_button, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
 
     @logger.catch
-    def get_log_file(self):
+    def get_server_from_log_file(self):
         """Поиск ip сервера в log файле"""
-        log_file = QFileDialog.getOpenFileName(self, 'Открыть файл')[0]
+        log_file = QFileDialog.getOpenFileName(self, 'Открыть файл', filter="log(*.log)")[0]
+        if not log_file:
+            return None
         with open(file=log_file, mode="r") as file:
+            content = file.read()
+        content_split = content.split("\n")
+        result_list = [string for string in content_split if
+                       "Enter to the 'Connected' state" in string]
+        result_list_len = len(result_list) - 1
+        last_connection_log = result_list[result_list_len]
+        self.last_log = log_file
+        server_address = last_connection_log.split(" ")
+        server_address = server_address[8:]
+        server_address = server_address[0]
+        server_address = server_address.split(",")
+        server_address = server_address[0]
+        server_address = server_address.split(":")
+        server_address = server_address[0]
+        logger.info(f"Slice result - {server_address}")
+        return server_address
+
+    @logger.catch
+    def get_server_info(self):
+        """Получение информации о сервере"""
+        server_address = self.get_server_from_log_file()
+        if not server_address and self.help_text_status is False:
+            self.help_text.setText("Не выбран файл")
+            return None
+        if not server_address and self.help_text_status is True:
+            self.result_text.setText("Не выбран файл")
+            return None
+        response = requests.get(url=f"https://ipinfo.io/{server_address}/json")
+        data = response.json()
+        country = data.get("country")
+        city = data.get("city")
+        if self.help_text_status is False:
+            self.destroy_help_text()
+        self.layout.addWidget(self.result_text)
+        self.result_text.setText(f"Город - {city}, Страна - {country}, IP сервера - {server_address}")
+
+    @logger.catch
+    def get_last_log(self):
+        with open(file=self.last_log, mode="r") as file:
             content = file.read()
         content_split = content.split("\n")
         result_list = [string for string in content_split if
@@ -38,23 +88,17 @@ class SearchLogWidget(QtWidgets.QTableWidget):
         server_address = server_address.split(":")
         server_address = server_address[0]
         logger.info(f"Slice result - {server_address}")
-        return server_address
-
-    @logger.catch
-    def get_server_info(self):
-        """Получение информации о сервере"""
-        server_address = self.get_log_file()
         response = requests.get(url=f"https://ipinfo.io/{server_address}/json")
         data = response.json()
         country = data.get("country")
         city = data.get("city")
         if self.help_text_status is False:
             self.destroy_help_text()
-        self.layout.addWidget(self.text)
-        self.text.setText(f"Город - {city}, Страна - {country}, IP сервера - {server_address}")
+        self.layout.addWidget(self.result_text)
+        self.result_text.setText(f"Город - {city}, Страна - {country}, IP сервера - {server_address}")
 
     @logger.catch
     def destroy_help_text(self):
-        """Удаление информационного текста """
+        """Удаление информационного текста"""
         self.help_text.deleteLater()
         self.help_text_status = True
--- src/widgets/main_widget.py
+++ src/widgets/main_widget.py
@@ -1,10 +1,12 @@
 from PySide6 import QtWidgets
-from src.widgets.log_search_widget import SearchLogWidget
+
+from src.widgets.log_search_widget import ServerAnalyzeWidget
 
 
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.log_widget = SearchLogWidget()
-        self.setCentralWidget(self.log_widget)
+        self.analyze_widget = ServerAnalyzeWidget()
+        self.setCentralWidget(self.analyze_widget)
+
