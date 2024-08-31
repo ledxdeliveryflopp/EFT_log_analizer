d0c76912bcff2e82df5c7c64a4829002ec1cdf6b
ledxdeliveryflopp <korstim18@gmail.com>
2024-08-31 23:38:21 +0700

    add ping server func, add multithreading

--- src/widgets/main_widget.py
+++ src/widgets/main_widget.py
@@ -32,15 +32,17 @@ class MainWindow(QtWidgets.QMainWindow):
             self.translation.setText("Перевод на английский")
 
             self.analyze_widget.help_text.setText("Название лога: network-connection")
-            self.analyze_widget.search_log_button.setText("Найти лог файл")
             self.analyze_widget.last_log_button.setText("Информация с последнего лога")
+            if self.analyze_widget.help_text_status is False:
+                self.analyze_widget.search_log_button.setText("Найти лог файл")
             self.setCentralWidget(self.analyze_widget)
         if self.translation.isChecked() is False:
             AppSettings.translations = False
             logger.info(f"translation off, button: {self.translation.isChecked()},"
                         f" settings: {AppSettings.translations}")
             self.translation.setText("Translation into russian")
-            self.analyze_widget.help_text.setText("log name: network-connection")
             self.analyze_widget.search_log_button.setText("Find log file")
             self.analyze_widget.last_log_button.setText("Get server from last log file")
+            if self.analyze_widget.help_text_status is False:
+                self.analyze_widget.help_text.setText("log name: network-connection")
             self.setCentralWidget(self.analyze_widget)
--- src/widgets/server_analyze_widget.py
+++ src/widgets/server_analyze_widget.py
@@ -1,10 +1,13 @@
 import subprocess
+from functools import partial
 import requests
 
 from PySide6 import QtWidgets, QtCore
 from PySide6.QtCore import QThreadPool
 from PySide6.QtWidgets import QFileDialog
 from loguru import logger
+
+from src.settings.settings import AppSettings
 from src.settings.utils import translate_country
 
 
@@ -12,15 +15,20 @@ class ServerAnalyzeWidget(QtWidgets.QWidget):
 
     def __init__(self):
         super().__init__()
+        self.thread_manager = QThreadPool()
+        logger.info(f"thread count - {self.thread_manager.maxThreadCount()}")
+
         self.help_text_status = False
+        self.ping_result_status = False
         self.last_log = None
+        self.last_ping = None
+
         self.layout = QtWidgets.QVBoxLayout(self)
 
         self.result_text = QtWidgets.QLabel(alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
         self.ping_result = QtWidgets.QLabel(alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
         self.help_text = QtWidgets.QLabel("log name: network-connection",
                                           alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
-
         self.search_log_button = QtWidgets.QPushButton("Find log file")
         self.last_log_button = QtWidgets.QPushButton("Get server from last log file")
 
@@ -34,7 +42,7 @@ class ServerAnalyzeWidget(QtWidgets.QWidget):
     @logger.catch
     def get_server_from_log_file(self):
         """Поиск ip сервера в log файле"""
-        log_file = QFileDialog.getOpenFileName(self, 'Открыть файл', filter="log(*.log)")[0]
+        log_file = QFileDialog.getOpenFileName(self, 'open file', filter="log(*.log)")[0]
         if not log_file:
             return None
         with open(file=log_file, mode="r") as file:
@@ -56,31 +64,53 @@ class ServerAnalyzeWidget(QtWidgets.QWidget):
         return server_address
 
     @logger.catch
-    def ping_server(self, server: str):
-        command = subprocess.run(["ping", f"{server}"], stdout=subprocess.PIPE, text=True,
-                                 encoding="cp866")
+    def ping_server(self, server_ip):
+        """Пинг сервера"""
+        logger.info(f"{self.ping_server.__name__} - thread start")
+        command = subprocess.run(["ping", f"{server_ip}"], stdout=subprocess.PIPE, text=True,
+                                 encoding="cp866", creationflags=subprocess.CREATE_NO_WINDOW)
         logger.info(f"ping status code - {command.returncode}")
         command_result_split = command.stdout.split("\n")
         command_result_slice = command_result_split[8:]
         command_result = command_result_slice[0]
         logger.info(f"ping result - {command_result}")
+        if self.ping_result_status is True:
+            self.ping_result.show()
         self.layout.addWidget(self.ping_result)
-        self.ping_result.setText(f"{command_result}")
+        self.last_ping = command_result
+        if not AppSettings.translations:
+            self.ping_result.setText(f"Ping-result  -{command_result}")
+        else:
+            self.ping_result.setText(f"Результаты пинга  -{command_result}")
+        logger.info(f"{self.ping_server.__name__} - thread stop")
 
     @logger.catch
     def get_server_info(self):
         """Получение информации о сервере"""
-        server_address = self.get_server_from_log_file()
-        self.ping_server(server=server_address)
-        if not server_address and self.help_text_status is False:
-            self.help_text.setText("Не выбран файл")
-            return None
+        server_ip = self.get_server_from_log_file()
+        if not server_ip and self.help_text_status is False:
+            if not AppSettings.translations:
+                self.help_text.setText("file dont selected")
+                self.destroy_ping_result()
+                return None
+            else:
+                self.help_text.setText("Файл не выбран")
+                self.destroy_ping_result()
+                return None
         if self.help_text_status is False:
             self.destroy_help_text()
-        if not server_address and self.help_text_status is True:
-            self.result_text.setText("Не выбран файл")
-            return None
-        response = requests.get(url=f"https://ipinfo.io/{server_address}/json")
+        if not server_ip and self.help_text_status is True:
+            if not AppSettings.translations:
+                self.result_text.setText("file dont selected")
+                self.destroy_ping_result()
+                return None
+            else:
+                self.result_text.setText("Файл не выбран")
+                self.destroy_ping_result()
+                return None
+        self.thread_manager.start(partial(self.ping_server, f"{server_ip}"))
+        logger.info(f"active thread count - {self.thread_manager.activeThreadCount()}")
+        response = requests.get(url=f"https://ipinfo.io/{server_ip}/json")
         data = response.json()
         country = data.get("country")
         city = data.get("city")
@@ -88,10 +118,10 @@ class ServerAnalyzeWidget(QtWidgets.QWidget):
         self.layout.addWidget(self.result_text)
         if not translated_country_name:
             self.result_text.setText(
-                f"City - {city}, Country - {country}, server IP - {server_address}")
+                f"City - {city}, Country - {country}, server IP - {server_ip}")
         if translated_country_name:
             self.result_text.setText(
-                f"Город - {city}, Страна - {translated_country_name}, IP сервера - {server_address}")
+                f"Город - {city}, Страна - {translated_country_name}, IP сервера - {server_ip}")
 
     @logger.catch
     def get_last_log(self):
@@ -130,3 +160,9 @@ class ServerAnalyzeWidget(QtWidgets.QWidget):
         """Удаление информационного текста"""
         self.help_text.deleteLater()
         self.help_text_status = True
+
+    @logger.catch
+    def destroy_ping_result(self):
+        """Удаление информационного текста"""
+        self.ping_result.hide()
+        self.ping_result_status = True
