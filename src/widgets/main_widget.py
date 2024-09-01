78409703ab81628b0a2e51b1969fc7bbb7291e9d
ledxdeliveryflopp <korstim18@gmail.com>
2024-09-01 21:26:11 +0700

    add block translation button if there is active thread, change raw text to dict from const

--- src/widgets/main_widget.py
+++ src/widgets/main_widget.py
@@ -4,10 +4,11 @@ from PySide6.QtWidgets import QToolBar
 from loguru import logger
 
 from src.settings.settings import AppSettings, get_translated_dict
+from src.settings.thread_manager import ThreadManager
 from src.widgets.server_analyze_widget import ServerAnalyzeWidget
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QtWidgets.QMainWindow, ThreadManager):
 
     def __init__(self):
         super().__init__()
@@ -24,27 +25,33 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def set_translation(self):
         """Перевод приложения на русский или английский"""
-        if self.translation.isChecked() is True:
-            AppSettings.translations = True
-            new_lang = get_translated_dict()
-            analyze_widget = ServerAnalyzeWidget()
-            logger.info(f"translation on, button: {self.translation.isChecked()},"
-                        f" settings: {AppSettings.translations}")
-            self.translation.setText(new_lang.get("translation"))
-            analyze_widget.search_log_button.setText(new_lang.get("search_log_file"))
-            analyze_widget.last_log_button.setText(new_lang.get("last_server"))
-            if analyze_widget.help_text_status is False:
-                analyze_widget.help_text.setText(new_lang.get("help_text"))
-            self.setCentralWidget(analyze_widget)
-        if self.translation.isChecked() is False:
-            AppSettings.translations = False
-            new_lang = get_translated_dict()
-            analyze_widget = ServerAnalyzeWidget()
-            logger.info(f"translation off, button: {self.translation.isChecked()},"
-                        f" settings: {AppSettings.translations}")
-            self.translation.setText(new_lang.get("translation"))
-            analyze_widget.search_log_button.setText(new_lang.get("search_log_file"))
-            analyze_widget.last_log_button.setText(new_lang.get("last_server"))
-            if analyze_widget.help_text_status is False:
-                analyze_widget.help_text.setText(new_lang.get("help_text"))
-            self.setCentralWidget(analyze_widget)
+        active_thread = self.get_active_thread_count()
+        if active_thread == 0:
+            if self.translation.isChecked() is True:
+                """Русский язык"""
+                AppSettings.translations = True
+                new_lang = get_translated_dict()
+                logger.info(f"translation on, button: {self.translation.isChecked()},"
+                            f" settings: {AppSettings.translations}")
+                self.translation.setText(new_lang.get("translation"))
+                analyze_widget = ServerAnalyzeWidget()
+                analyze_widget.search_log_button.setText(new_lang.get("search_log_file"))
+                analyze_widget.last_log_button.setText(new_lang.get("last_server"))
+                if analyze_widget.help_text_status is False:
+                    analyze_widget.help_text.setText(new_lang.get("help_text"))
+                self.setCentralWidget(analyze_widget)
+            if self.translation.isChecked() is False:
+                """Английский язык"""
+                AppSettings.translations = False
+                new_lang = get_translated_dict()
+                logger.info(f"translation off, button: {self.translation.isChecked()},"
+                            f" settings: {AppSettings.translations}")
+                self.translation.setText(new_lang.get("translation"))
+                analyze_widget = ServerAnalyzeWidget()
+                analyze_widget.search_log_button.setText(new_lang.get("search_log_file"))
+                analyze_widget.last_log_button.setText(new_lang.get("last_server"))
+                if analyze_widget.help_text_status is False:
+                    analyze_widget.help_text.setText(new_lang.get("help_text"))
+                self.setCentralWidget(analyze_widget)
+        else:
+            self.translation.setChecked(False)
