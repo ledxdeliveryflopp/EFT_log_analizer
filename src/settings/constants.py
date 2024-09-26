026c193860166a371c7e1578568ca179ee973008
ledxdeliveryflopp <korstim18@gmail.com>
2024-09-26 20:53:23 +0700

    delete translation UI dict and dict return func

--- src/settings/constants.py
+++ src/settings/constants.py
@@ -3,31 +3,3 @@ class Translation:
     COUNTRY: dict = {"DK": "Дания", "RU": "Россия", "GB": "Великобритания",
                      "FI": "Финляндия", "SE": "Швеция", "IE": "Ирландия",
                      "DE": "Германия", "CH": "Швейцария"}
-
-    ENG_UI: dict = {"app_toolbar": "App", "translation": "Translation into russian",
-                    "search_log_file": "Find log file",
-                    "last_server": "Get server from last log file",
-                    "help_text": "log name: network-connection", "settings": "Settings",
-                    "version_checker": "Version checker", "server_ping": "Enable server ping"}
-    ENG_FUNC: dict = {"search_file": "open file", "ping_result": "Ping-result  -",
-                      "file_dont_selected": "file dont selected", "city": "City -",
-                      "country": "Country -", "server_ip": "server IP -",
-                      "outdated_version": "Outdated version", "last_version": "Last version",
-                      "actual_version": "Actual version - ", "app_version": "App version - ",
-                      "github_releases": "github - ", "releases_link": "Link to actual releases"
-                      }
-
-    RU_UI: dict = {"app_toolbar": "Приложение", "translation": "Перевод на нанглийский",
-                   "search_log_file": "Поиск log файла",
-                   "last_server": "Сервер из последнего лога",
-                   "help_text": "Название лога: network-connection",
-                   "settings": "Настройки", "version_checker": "Проверка версии",
-                   "server_ping": "Включить пинг сервера"}
-    RU_FUNC: dict = {"search_file": "открыть файл",
-                     "ping_result": "Результаты пинга  -",
-                     "file_dont_selected": "Файл не выбран", "city": "Город -",
-                     "country": "Страна -", "server_ip": "IP сервера -",
-                     "outdated_version": "Устаревшая версия", "last_version": "Последняя версия",
-                     "actual_version": "Актуальная версия - ",
-                     "app_version": "Версия приложения - ", "github_releases": "github - ",
-                     "releases_link": "Ссылка на актуальный релиз"}
--- src/settings/settings.py
+++ src/settings/settings.py
@@ -1,32 +1,5 @@
-from src.settings.constants import Translation
 
 
 class AppSettings:
-    translations: bool = False
     server_ping: bool = False
-    app_version: str = "V6.0"
-
-    def get_ui_translation(self) -> dict:
-        """Возврат словаря с переводом ui"""
-        if self.translations is False:
-            return Translation.ENG_UI
-        elif self.translations is True:
-            return Translation.RU_UI
-
-    def get_func_translation(self) -> dict:
-        if self.translations is False:
-            return Translation.ENG_FUNC
-        elif self.translations is True:
-            return Translation.RU_FUNC
-
-
-def get_translated_dict() -> dict:
-    settings = AppSettings()
-    ui_dict = settings.get_ui_translation()
-    return ui_dict
-
-
-def get_translated_func() -> dict:
-    settings = AppSettings()
-    func_dict = settings.get_func_translation()
-    return func_dict
+    app_version: str = "V5.0"
